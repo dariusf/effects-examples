5ef57fdc5e8041b75457dcc81a8862ace5b9d8e2
Darius Foo <darius.foo.tw@gmail.com>
2021-07-13 22:55:26 +0800

    Attempts at state

--- state.ml
+++ state.ml
@@ -12,23 +12,30 @@ module State (S : sig type t end) : STATE with type t = S.t = struct
   type t = S.t
 
   effect Put : t -> unit
+  (* post = Put *)
   let put v = perform (Put v)
 
   effect Get : t
+  (* post = Get *)
   let get () = perform Get
 
   let run f ~init =
     let comp =
       match f () with
       | x -> (fun s -> (s, x))
-      | effect (Put s') k -> (fun _s -> continue k () s')
-      | effect Get k -> (fun s -> continue k s s)
+      | effect (Put s') k ->
+        (* policy: Put -> *)
+        (fun _s -> continue k () s')
+      | effect Get k ->
+        (* policy: Get -> *)
+        (fun s -> continue k s s)
     in comp init
 end
 
 module IS = State (struct type t = int end)
 module SS = State (struct type t = string end)
 
+(* post = IS.Get . IS.Put . IS.Get... *)
 let foo () : unit =
   printf "%d\n" (IS.get ());
   IS.put 42;
@@ -40,4 +47,5 @@ let foo () : unit =
   SS.put "world";
   printf "%s\n" (SS.get ())
 
+(* higher order *)
 let _ = IS.run (fun () -> SS.run foo ~init:"") ~init:0
